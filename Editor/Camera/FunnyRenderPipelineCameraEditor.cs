7c72c55c906dd0080c88eae012cba2ededf09ca2
lixiaoyu <lixiaoyu@xmfunny.com>
2023-04-10 17:27:55 +0800

    <feature> 代码优化

--- Editor/Camera/FunnyRenderPipelineCameraEditor.cs
+++ Editor/Camera/FunnyRenderPipelineCameraEditor.cs
@@ -1,10 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering.SoFunny;
-using UnityEditor.Rendering.Universal;
-using UnityEditor.SceneManagement;
 using UnityEditorInternal;
 
 namespace UnityEditor.Rendering.SoFunny {
@@ -13,12 +9,6 @@ namespace UnityEditor.Rendering.SoFunny {
     [CustomEditorForRenderPipeline(typeof(Camera), typeof(FunnyRenderPipelineAsset))]
     [CanEditMultipleObjects]
     public class FunnyRenderPipelineCameraEditor : CameraEditor {
-        // Unity GUI工具 快速创建可排序的列表
-        ReorderableList m_LayerList;
-        List<Camera> validCameras = new List<Camera>();
-        List<Camera> m_TypeErrorCameras = new List<Camera>();
-        List<Camera> m_NotSupportedOverlayCameras = new List<Camera>();
-        List<Camera> m_IncompatibleCameras = new List<Camera>();
         List<(Camera, FunnyRenderPipelineSerializedCamera)> m_OutputWarningCameras = new();
         FunnyRenderPipelineSerializedCamera m_SerializedCamera;
         public new void OnEnable() {
@@ -26,13 +16,6 @@ namespace UnityEditor.Rendering.SoFunny {
             settings.OnEnable();
             //selectedCameraInStack = null;
             m_SerializedCamera = new FunnyRenderPipelineSerializedCamera(serializedObject, settings);
-
-            validCameras.Clear();
-            m_TypeErrorCameras.Clear();
-            m_NotSupportedOverlayCameras.Clear();
-            m_IncompatibleCameras.Clear();
-            m_OutputWarningCameras.Clear();
-
             UpdateCameras();
 
             Undo.undoRedoPerformed += ReconstructReferenceToAdditionalDataSO;
@@ -49,45 +32,6 @@ namespace UnityEditor.Rendering.SoFunny {
         
         void UpdateCameras() {
             m_SerializedCamera.Refresh();
-
-            // UI
-            // m_LayerList = new ReorderableList(m_SerializedCamera.serializedObject, m_SerializedCamera.cameras, true, true, true, true) {
-            //     drawHeaderCallback = rect => EditorGUI.LabelField(rect, Styles.cameras),
-            //     drawElementCallback = DrawElementCallback,
-            //     onSelectCallback = SelectElement,
-            //     onRemoveCallback = RemoveCamera,
-            //     onCanRemoveCallback = CanRemoveCamera,
-            //     onAddDropdownCallback = AddCameraToCameraList
-            // };
-        }
-
-        /// <summary>
-        /// 判断相机是否可以删除
-        /// </summary>
-        bool CanRemoveCamera(ReorderableList list) => m_SerializedCamera.numCameras > 0;
-
-        /// <summary>
-        /// 用于绘制每个相机的信息
-        /// </summary>
-        void DrawElementCallback(Rect rect, int index, bool isActive, bool isFocused) {
-        }
-
-        /// <summary>
-        /// 选择相机
-        /// </summary>
-        void SelectElement(ReorderableList list) {
-        }
-
-        /// <summary>
-        /// 删除相机时回调，用于清空相机信息
-        /// </summary>
-        void RemoveCamera(ReorderableList list) {
-        }
-
-        /// <summary>
-        /// 添加相机
-        /// </summary>
-        void AddCameraToCameraList(Rect rect, ReorderableList list) {
         }
     }
 }
